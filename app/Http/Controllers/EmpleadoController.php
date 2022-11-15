a1aecbf9ec6f097b96cbb65d62817acea5fe16c9
RichardManques <manques11@gmail.com>
2022-11-14 22:11:01 -0300

    Modellbook laravel

---
+++ app/Http/Controllers/EmpleadoController.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Empleado;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+
+class EmpleadoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $datos['empleados'] = Empleado::paginate(5);
+        return view('empleado.index',$datos);//-> la variable $datos se debe pasar a la vista especificado como variable
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('empleado.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //$datosEmpleado = request()->all();
+        $campos=[
+            'Nombre'=>'required|string|max:100',
+            'ApellidoPaterno'=>'required|string|max:100',
+            'ApellidoMaterno'=>'required|string|max:100',
+            'Correo'=>'required|email',
+            'Foto'=>'required|max:10000|mimes:jpeg,png,jpg'
+        ];
+        $mensaje=[
+            'required'=>'El :attribute es requerido',
+            'Foto.required'=>'La foto es requerida'
+        ];
+        $this->validate($request,$campos,$mensaje);
+
+
+        $datosEmpleado = request()->except('_token');
+        if($request->hasFile('Foto')){
+            $datosEmpleado['Foto']=$request->file('Foto')->store('uploads','public');
+        }
+        Empleado::insert($datosEmpleado);
+        
+        return redirect('empleado')->with('mensaje','Empleado creado con éxito');
+        //return response()->json($datosEmpleado);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Empleado  $empleado
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Empleado $empleado)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Empleado  $empleado
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //recuperar los datos de la vista
+        $empleado= Empleado::findOrFail($id);
+        return view('empleado.edit',compact('empleado') );//retornalo a la vista editar empleado
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Empleado  $empleado
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //metodo para actualizar los datos para el empleado en la base de datos
+        $datosEmpleado = request()->except(['_token','_method']);
+        if($request->hasFile('Foto')){
+            $empleado = Empleado::findOrFail($id);
+            Storage::delete('public/'.$empleado->Foto);
+            $datosEmpleado['Foto']=$request->file('Foto')->store('uploads','public');
+        }
+
+        Empleado::where('id','=',$id)->update($datosEmpleado);
+        $empleado = Empleado::findOrFail($id);
+        return view('empleado.edit',compact('empleado'));//retornalo a la vista editar empleado
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Empleado  $empleado
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)//eliminar un empleado
+    {
+        $empleado = Empleado::findOrFail($id);
+
+        //preguntar si la foto existe, si no ,se borra
+        if(Storage::delete('public/'.$empleado->Foto)){
+            Empleado::destroy($id);
+        }
+        
+        return redirect('empleado')->with('mensaje','Empleado eliminado');
+    }
+}
---
+++ resources/views/empleado/index.blade.php
@@ -0,0 +1,51 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+
+@if(Session::has('mensaje'))
+{{ Session::get('mensaje') }}
+@endif
+<a href="{{ url('empleado/create') }}" class="btn btn-success">Agregar empleado</a>
+<table class="table table-light">
+    <thead class="thead-light">
+        <tr>
+            <th>#</th>
+            <th>Foto</th>
+            <th>Nombre</th>
+            <th>A.Paterno</th>
+            <th>A.Materno</th>
+            <th>Correo</th>
+            <th>Acciones</th>
+        </tr>
+    </thead>
+    
+    <tbody>
+        @foreach($empleados as $empleado)
+        <tr>
+            <td>{{ $empleado->id }}</td>
+            <td>
+                <img class="img-thumbnail img-fluid" src="{{ asset('storage').'/'.$empleado->Foto }}" width="100" alt="">
+            </td>
+            <td>{{ $empleado->Nombre }}</td>
+            <td>{{ $empleado->ApellidoPaterno }}</td>
+            <td>{{ $empleado->ApellidoMaterno }}</td>
+            <td>{{ $empleado->Correo }}</td>
+            <td>
+                <a href="{{ url('/empleado/'.$empleado->id.'/edit') }}" class="btn btn-warning">
+                    Editar
+                </a>
+                |
+                <form action="{{ url('/empleado/'.$empleado->id) }}" method="POST" class="d-inline">
+                    @csrf
+                    {{ method_field('DELETE') }}
+                    <input class="btn btn-danger" type="submit" onclick="return confirm('¿Quieres borrar?')" 
+                    value="Borrar">
+                </form>
+            </td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
+</div>
+@endsection
